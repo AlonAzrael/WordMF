b29d34efc56da5161e251672ae7325c59638d44c
Your Name <you@email.com>
2016-06-26 16:38:42 +0800

    add gpu

--- glove_gpu/glove_cuda.py
+++ glove_gpu/glove_cuda.py
@@ -12,44 +12,84 @@ import time as t
 from jinja2 import Template
 
 
-def gen_glove_cuda_kernel():
-    
-    learning_rate = 0.05
-    max_loss = 10.0
+def gen_glove_cuda_kernel(block_dim, grid_dim, n_features=10, learning_rate=0.05, max_loss=10, k_loss=2):
     
     glove_cuda_kernel_template = """
     #include <stdio.h>
+    #include <math.h>
+
+    #define BLOCK_SIZE {{block_size}}
+    #define GRID_DIMX {{grid_dimx}}
+    #define BLOCK_DIMX {{block_dimx}}
+    #define N_FEATURES {{n_features}}
+    #define K_LOSS {{k_loss}}
 
     __global__ void glove_kernel(
         // data part
-        int* shuffle_indices,
         int* rows, 
         int* cols, 
-        float* datas, 
-        int datas_size
+        float* datas, int datas_size,
+        int* shuffle_indices,
         // nn part
         /*
-        float* word_vectors, 
-        float* wordvec_sum_gradients, 
-        float* word_biases, 
-        float* wordbias_sum_gradients, 
+        float* word_biases, float* wordbias_sum_gradients, 
         */
+        float* word_vectors, float* wordvec_sum_gradients
         ){
 
-    // float learning_rate = {{learning_rate}};
-    // float max_loss = {{max_loss}};
+    float init_learning_rate = {{learning_rate}};
+    float max_loss = {{max_loss}};
 
     // distribute index
-    int k = threadIdx.x + blockIdx.x * blockDim.x;
+    // int k = threadIdx.x + blockIdx.x * blockDim.x;
+    int kk = blockIdx.y*GRID_DIMX*BLOCK_SIZE+blockIdx.x*BLOCK_SIZE+threadIdx.y*BLOCK_DIMX+threadIdx.x;
 
-    if (k < datas_size) {
-        int shuffle_index = shuffle_indices[k];
+    if (kk < datas_size) {
+        int shuffle_index = shuffle_indices[kk];
         int word_a = rows[shuffle_index];
         int word_b = cols[shuffle_index];
         float val = datas[shuffle_index];
+        
+        float prediction = 0.0;
+        float loss = 0.0;
+        float learning_rate = init_learning_rate;
+        float gradient = 0.0;
 
-        printf("I am %dth thread in threadIdx.x:%d.threadIdx.y:%d  blockIdx.:%d blockIdx.y:%d blockDim.x:%d blockDim.y:%d\\n",(threadIdx.x+threadIdx.y*blockDim.x+(blockIdx.x*blockDim.x*blockDim.y)+(blockIdx.y*blockDim.x*blockDim.y)),threadIdx.x, threadIdx.y,blockIdx.x,blockIdx.y,blockDim.x,blockDim.y);
-        printf("val:%f", val);
+        // printf("I am %dth thread", kk);
+        // printf("k:%d\\n", kk);
+        // printf("shuffle_index:%d\\n", shuffle_index);
+
+        // === start one update ===
+        int ia = word_a*N_FEATURES;
+        int ib = word_b*N_FEATURES;
+
+        for (int i=0;i<N_FEATURES;i++) {
+            prediction = prediction + word_vectors[ia+i] * word_vectors[ib+i];
+        }
+
+        loss = (prediction - val) * K_LOSS;
+        if (loss < -max_loss) 
+            loss = -max_loss;
+        else if (loss > max_loss)
+            loss = max_loss;
+
+        for (int i=0;i<N_FEATURES;i++) {
+            
+            // update word_a
+            learning_rate = init_learning_rate / sqrt(wordvec_sum_gradients[ia+i]);
+            gradient = loss * word_vectors[ib+i];
+            word_vectors[ia+i] = (word_vectors[ia+i] - learning_rate*gradient);
+            wordvec_sum_gradients[ia+i] += gradient*gradient;
+
+            // update word_b
+            learning_rate = init_learning_rate / sqrt(wordvec_sum_gradients[ib+i]);
+            gradient = loss * word_vectors[ia+i];
+            word_vectors[ib+i] = (word_vectors[ib+i] - learning_rate*gradient);
+            wordvec_sum_gradients[ib+i] += gradient*gradient;
+
+        }
+        // === end one update ===
+
     } 
     else {
         return;
@@ -57,32 +97,77 @@ def gen_glove_cuda_kernel():
     
     }
     """
-    glove_cuda_kernel_s = Template(glove_cuda_kernel_template).render(learning_rate=learning_rate, max_loss=max_loss)
+    glove_cuda_kernel_s = Template(glove_cuda_kernel_template).render(
+        
+        learning_rate=learning_rate, 
+        max_loss=max_loss, 
+        n_features=n_features,
+        k_loss=k_loss,
+
+        block_dimx=block_dim[0],
+        block_dimy=block_dim[1],
+        block_size=block_dim[0]*block_dim[1],
+        grid_dimx=grid_dim[0],
+        grid_dimy=grid_dim[1],
+    )
 
     return glove_cuda_kernel_s
 
 
 def run_glove_gpu():
-    glove_cuda_kernel_s = gen_glove_cuda_kernel()
+    
+    datas_size = 1024
+    n_features = 10
+    n_words = datas_size
+
+    
+    # cuda init
+    
+    block_dim = [4,4]
+    grid_dim = [2,2]
+    glove_cuda_kernel_s = gen_glove_cuda_kernel(block_dim, grid_dim, n_features=n_features)
+    print glove_cuda_kernel_s
     
     mod = SourceModule(glove_cuda_kernel_s)
     glove_kernel = mod.get_function("glove_kernel")
 
-    datas_size = 100
-    shuffle_indices = np.arange(datas_size, dtype=np.int32)
+    
+    # data init
+    
     rows = np.arange(datas_size, dtype=np.int32)
     cols = np.arange(datas_size, dtype=np.int32)
     # datas = random.random(datas_size, dtype=np.float32)
     datas = np.arange(datas_size, dtype=np.float32)
+
+    shuffle_indices = random.permutation(datas_size).astype("int32")
+
     
-    gshuffle_indices = gpuarray.to_gpu(shuffle_indices)
+    # model init
+    
+    word_vectors = (random.random((n_words, n_features), ).astype('float32') - 0.5) / n_features
+    wordvec_sum_gradients = np.ones_like(word_vectors)
+    
+    word_biases = np.zeros(n_words, dtype=np.float32)
+    wordbias_sum_gradients = np.ones_like(word_biases)
+    
+    
+    # params to gpu
+
     grows = gpuarray.to_gpu(rows)
     gcols = gpuarray.to_gpu(cols)
     gdatas = gpuarray.to_gpu(datas)
 
-    glove_kernel(gshuffle_indices, grows, gcols, gdatas, np.int32(datas_size), block=(4,4,1), grid=(2,2,1))
+    gword_vectors = gpuarray.to_gpu(word_vectors)
+    gwordvec_sum_gradients = gpuarray.to_gpu(wordvec_sum_gradients)
+    
+    gshuffle_indices = gpuarray.to_gpu(shuffle_indices)
 
-
+    glove_kernel(
+        grows, gcols, gdatas, np.int32(datas_size),
+        gshuffle_indices,
+        gword_vectors, gwordvec_sum_gradients,
+        block=tuple(block_dim+[1]), grid=tuple(grid_dim+[1]),
+    )
 
 
 
